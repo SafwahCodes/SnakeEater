8d507ad0167a2eaceabaa39890104d4caab608c8
KazuNekoCHAN13 <nurdiyanahsafwah@gmail.com>
2020-06-21 13:34:40 +0800

    Added snake movement and pass through walls to the other side

--- venv/Include/main.py
+++ venv/Include/main.py
@@ -1,51 +1,69 @@
 import pygame
 
-
-
-def drawBackground():
-    for i in range(0, x, 20):
-        pygame.draw.rect(gameDisplay,pygame.Color(255,255,255,255), pygame.Rect(i, 0, 1, y))
-    for j in range(0, y, 20):
-        pygame.draw.rect(gameDisplay,pygame.Color(255,255,255,255), pygame.Rect(0, j, x, 1))
-
-def drawSnake():
-    pygame.draw.rect(gameDisplay, pygame.Color(255, 255, 255, 255), pygame.Rect(snake_x, snake_y, 20, 20))
-
 pygame.init()
-
-x = 420
-y = 300
+screen_x = 420
+screen_y = 300
+gameDisplay = pygame.display.set_mode((screen_x,screen_y))
+pygame.display.set_caption("Snake Eater")
+clock = pygame.time.Clock()
+gameOver = False
 
 snake_x = 200
 snake_y = 140
 width_height = 20
+movement = "up"
 
-gameDisplay = pygame.display.set_mode((x,y))
-pygame.display.set_caption("Snake Eater")
+def drawBackground():
+    global screen_x, screen_y
+    for i in range(0, screen_x, 20):
+        pygame.draw.rect(gameDisplay,pygame.Color(255,255,255,255), pygame.Rect(i, 0, 1, screen_y))
+    for j in range(0, screen_y, 20):
+        pygame.draw.rect(gameDisplay,pygame.Color(255,255,255,255), pygame.Rect(0, j, screen_x, 1))
 
-clock = pygame.time.Clock()
+def drawSnake():
+    global snake_x, snake_y
+    if (snake_x < 0):
+        snake_x = screen_x - width_height
+    if (snake_x > screen_x - width_height):
+        snake_x = 0
+    if (snake_y < 0):
+        snake_y = screen_y - width_height
+    if (snake_y > screen_y - width_height):
+        snake_y = 0
+    pygame.draw.rect(gameDisplay, pygame.Color(255, 255, 255, 255), pygame.Rect(snake_x, snake_y, 20, 20))
 
-gameOver = False
+def snakeMove():
+    global snake_x, snake_y
+    if (movement == "right"):
+        snake_x += width_height
+    if (movement == "down"):
+        snake_y += width_height
+    if (movement == "left"):
+        snake_x -= width_height
+    if(movement == "up"):
+        snake_y -= width_height
 
+# game loop
 while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                snake_x += width_height
+                movement = "right"
             elif event.key == pygame.K_DOWN:
-                snake_y += width_height
+                movement = "down"
             elif event.key == pygame.K_LEFT:
-                snake_x -= width_height
+                movement = "left"
             elif event.key == pygame.K_UP:
-                snake_y -= width_height
+                movement = "up"
 
     gameDisplay.fill((0,0,0))
     drawBackground()
+    snakeMove()
     drawSnake()
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(5)
 
 pygame.quit()
 quit()
